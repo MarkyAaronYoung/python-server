dc9455cc183f850266a7cd1e207b5ef51b13eb1b
Mark Young <markyaaronyoung@gmail.com>
2020-10-21 19:49:25 -0500

    create new animal function posts a new animal to the database

--- kennel.sql
+++ kennel.sql
@@ -1,16 +1,11 @@
     SELECT
             a.id,
             a.name,
-            a.breed,
-            a.status,
-            a.location_id,
-            a.customer_id,
+            a.address,
+            a.location_id
             l.name location_name,
-            l.address location_address,
-            c.name customer_name,
-            c.address customer_address
-        FROM Animal a
-        JOIN Location l
-            ON l.id = a.location_id
-        JOIN Location c
-            on c.id = a.customer_id
+            l.address location_address
+    FROM employee a
+    JOIN Location l
+        ON l.id = a.location_id
+    
